3ac5c698b7719d7472cfb64a716267c5e05369f8
planit <planit@tanjeffreyz.github.io>
2022-11-04 17:43:02 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -216,6 +216,20 @@ assignments = {
       "dueDate": "2022-11-04T23:59:00",
       "link": "https://www.gradescope.com/courses/419636",
       "submitted": false
+    },
+    {
+      "title": "Homework 9 (Electronic)",
+      "course": "CS 188",
+      "dueDate": "2022-11-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/419636/assignments/2413098/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Homework 9 (Written)",
+      "course": "CS 188",
+      "dueDate": "2022-11-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/419636",
+      "submitted": false
     }
   ],
   "Data 101": [
