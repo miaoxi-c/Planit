0710d97321b0fff799a8d07f3abae576e3bafe7d
planit <planit@tanjeffreyz.github.io>
2022-12-06 05:36:58 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -235,8 +235,8 @@ assignments = {
       "title": "Project 5",
       "course": "CS 188",
       "dueDate": "2022-12-01T23:59:00",
-      "link": "https://www.gradescope.com/courses/419636",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/419636/assignments/2424042/submissions/152702324",
+      "submitted": true
     },
     {
       "title": "[Optional] Project 5 Cheatsheet",
