92ee2d0c2da05d8f3eff0d6265eb7a0c85108dd9
planit <planit@tanjeffreyz.github.io>
2023-01-01 02:12:55 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -3,336 +3,336 @@ assignments = {
     {
       "title": "Homework 0 (Electronic)",
       "course": "CS 188",
-      "dueDate": "2022-09-02T22:59:00",
+      "dueDate": "2023-09-02T22:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2191720/submissions/131223551",
       "submitted": true
     },
     {
       "title": "Project 0",
       "course": "CS 188",
-      "dueDate": "2022-08-30T23:59:00",
+      "dueDate": "2023-08-30T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2191764/submissions/131124499",
       "submitted": true
     },
     {
       "title": "Project 1",
       "course": "CS 188",
-      "dueDate": "2022-09-09T23:59:00",
+      "dueDate": "2023-09-09T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2214902/submissions/132083122",
       "submitted": true
     },
     {
       "title": "Homework 1 (Written)",
       "course": "CS 188",
-      "dueDate": "2022-09-09T23:59:00",
+      "dueDate": "2023-09-09T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2215244/submissions/132868266",
       "submitted": true
     },
     {
       "title": "[Ungraded] Project 1 Cheatsheet",
       "course": "CS 188",
-      "dueDate": "2022-12-31T23:59:00",
+      "dueDate": "2023-12-31T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2215872/submissions/131949093",
       "submitted": true
     },
     {
       "title": "[Ungraded] Project 2 Cheatsheet",
       "course": "CS 188",
-      "dueDate": "2022-12-31T23:59:00",
+      "dueDate": "2023-12-31T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2215896/submissions/134569291",
       "submitted": true
     },
     {
       "title": "[Ungraded] Project 3 Cheatsheet",
       "course": "CS 188",
-      "dueDate": "2022-12-31T23:59:00",
+      "dueDate": "2023-12-31T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2215910/submissions/new",
       "submitted": false
     },
     {
       "title": "Homework 1 (Electronic)",
       "course": "CS 188",
-      "dueDate": "2022-09-09T23:59:00",
+      "dueDate": "2023-09-09T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2216269/submissions/132083320",
       "submitted": true
     },
     {
       "title": "Homework 2 (Written)",
       "course": "CS 188",
-      "dueDate": "2022-09-16T23:59:00",
+      "dueDate": "2023-09-16T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2241422/submissions/134374935",
       "submitted": true
     },
     {
       "title": "Homework 2 (Electronic)",
       "course": "CS 188",
-      "dueDate": "2022-09-16T23:59:00",
+      "dueDate": "2023-09-16T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2244112/submissions/133157868",
       "submitted": true
     },
     {
       "title": "Project 2",
       "course": "CS 188",
-      "dueDate": "2022-09-22T23:59:00",
+      "dueDate": "2023-09-22T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2251698/submissions/134933933",
       "submitted": true
     },
     {
       "title": "Homework 3 (Electronic)",
       "course": "CS 188",
-      "dueDate": "2022-09-23T23:59:00",
+      "dueDate": "2023-09-23T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2264987/submissions/134797216",
       "submitted": true
     },
     {
       "title": "Homework 3 (Written)",
       "course": "CS 188",
-      "dueDate": "2022-09-23T23:59:00",
+      "dueDate": "2023-09-23T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2265006/submissions/136029331",
       "submitted": true
     },
     {
       "title": "Mini Contest 1",
       "course": "CS 188",
-      "dueDate": "2022-09-26T23:59:00",
+      "dueDate": "2023-09-26T23:59:00",
       "link": "https://www.gradescope.com/courses/419636",
       "submitted": false
     },
     {
       "title": "Homework 4 (Electronic)",
       "course": "CS 188",
-      "dueDate": "2022-09-30T23:59:00",
+      "dueDate": "2023-09-30T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2288351/submissions/136415781",
       "submitted": true
     },
     {
       "title": "Homework 4 (Written)",
       "course": "CS 188",
-      "dueDate": "2022-09-30T23:59:00",
+      "dueDate": "2023-09-30T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2288354/submissions/137609292",
       "submitted": true
     },
     {
       "title": "Project 3",
       "course": "CS 188",
-      "dueDate": "2022-10-04T23:59:00",
+      "dueDate": "2023-10-04T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2292647/submissions/138175236",
       "submitted": true
     },
     {
       "title": "Homework 5 (Electronic)",
       "course": "CS 188",
-      "dueDate": "2022-10-07T23:59:00",
+      "dueDate": "2023-10-07T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2311001/submissions/138393051",
       "submitted": true
     },
     {
       "title": "Homework 5 (Written)",
       "course": "CS 188",
-      "dueDate": "2022-10-07T23:59:00",
+      "dueDate": "2023-10-07T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2311005/submissions/139330003",
       "submitted": true
     },
     {
       "title": "Practice Midterm",
       "course": "CS 188",
-      "dueDate": "2022-10-12T17:00:00",
+      "dueDate": "2023-10-12T17:00:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2329139/submissions/140244391",
       "submitted": true
     },
     {
       "title": "Midterm (Online)",
       "course": "CS 188",
-      "dueDate": "2022-10-12T21:12:00",
+      "dueDate": "2023-10-12T21:12:00",
       "link": "https://www.gradescope.com/courses/419636",
       "submitted": false
     },
     {
       "title": "Homework 6 (Electronic)",
       "course": "CS 188",
-      "dueDate": "2022-10-21T23:59:00",
+      "dueDate": "2023-10-21T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2353470/submissions/142293104",
       "submitted": true
     },
     {
       "title": "Homework 6 (Written)",
       "course": "CS 188",
-      "dueDate": "2022-10-21T23:59:00",
+      "dueDate": "2023-10-21T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2353471/submissions/142856963",
       "submitted": true
     },
     {
       "title": "Participation (Extra Credit)",
       "course": "CS 188",
-      "dueDate": "2022-12-31T00:00:00",
+      "dueDate": "2023-12-31T00:00:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2356700/submissions/149324644",
       "submitted": true
     },
     {
       "title": "Homework 7 (Electronic)",
       "course": "CS 188",
-      "dueDate": "2022-10-28T23:59:00",
+      "dueDate": "2023-10-28T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2374071/submissions/144114642",
       "submitted": true
     },
     {
       "title": "Homework 7 (Written)",
       "course": "CS 188",
-      "dueDate": "2022-10-28T23:59:00",
+      "dueDate": "2023-10-28T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2374097/submissions/144779286",
       "submitted": true
     },
     {
       "title": "Project 4 Checkpoint",
       "course": "CS 188",
-      "dueDate": "2022-11-01T23:59:00",
+      "dueDate": "2023-11-01T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2380591/submissions/144176021",
       "submitted": true
     },
     {
       "title": "Project 4",
       "course": "CS 188",
-      "dueDate": "2022-11-08T23:59:00",
+      "dueDate": "2023-11-08T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2380617/submissions/147187847",
       "submitted": true
     },
     {
       "title": "[Ungraded] Project 4 Cheatsheet",
       "course": "CS 188",
-      "dueDate": "2022-12-31T23:59:00",
+      "dueDate": "2023-12-31T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2380803/submissions/143875510",
       "submitted": true
     },
     {
       "title": "Homework 8 (Electronic)",
       "course": "CS 188",
-      "dueDate": "2022-11-04T23:59:00",
+      "dueDate": "2023-11-04T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2393879/submissions/145161299",
       "submitted": true
     },
     {
       "title": "Homework 8 (Written)",
       "course": "CS 188",
-      "dueDate": "2022-11-04T23:59:00",
+      "dueDate": "2023-11-04T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2393880/submissions/146646669",
       "submitted": true
     },
     {
       "title": "Homework 9 (Electronic)",
       "course": "CS 188",
-      "dueDate": "2022-11-29T23:59:00",
+      "dueDate": "2023-11-29T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2413098/submissions/148594792",
       "submitted": true
     },
     {
       "title": "Homework 9 (Written)",
       "course": "CS 188",
-      "dueDate": "2022-11-29T23:59:00",
+      "dueDate": "2023-11-29T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2413103/submissions/149255042",
       "submitted": true
     },
     {
       "title": "Project 5",
       "course": "CS 188",
-      "dueDate": "2022-12-01T23:59:00",
+      "dueDate": "2023-12-01T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2424042/submissions/152702324",
       "submitted": true
     },
     {
       "title": "[Ungraded] Project 5 Cheatsheet",
       "course": "CS 188",
-      "dueDate": "2022-12-31T23:59:00",
+      "dueDate": "2023-12-31T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2424137/submissions/150778529",
       "submitted": true
     },
     {
       "title": "[Ungraded] HW1 Practice Copy",
       "course": "CS 188",
-      "dueDate": "2022-12-31T23:59:00",
+      "dueDate": "2023-12-31T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2480375/submissions/new",
       "submitted": false
     },
     {
       "title": "[Ungraded] HW2 Practice Copy",
       "course": "CS 188",
-      "dueDate": "2022-12-31T23:59:00",
+      "dueDate": "2023-12-31T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2480376/submissions/new",
       "submitted": false
     },
     {
       "title": "[Ungraded] HW3 Practice Copy",
       "course": "CS 188",
-      "dueDate": "2022-12-31T23:59:00",
+      "dueDate": "2023-12-31T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2480377/submissions/new",
       "submitted": false
     },
     {
       "title": "[Ungraded] HW4 Practice Copy",
       "course": "CS 188",
-      "dueDate": "2022-12-31T23:59:00",
+      "dueDate": "2023-12-31T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2480378/submissions/new",
       "submitted": false
     },
     {
       "title": "[Ungraded] HW5 Practice Copy",
       "course": "CS 188",
-      "dueDate": "2022-12-31T23:59:00",
+      "dueDate": "2023-12-31T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2480379/submissions/new",
       "submitted": false
     },
     {
       "title": "[Ungraded] HW6 Practice Copy",
       "course": "CS 188",
-      "dueDate": "2022-12-31T23:59:00",
+      "dueDate": "2023-12-31T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2480380/submissions/new",
       "submitted": false
     },
     {
       "title": "[Ungraded] HW7 Practice Copy",
       "course": "CS 188",
-      "dueDate": "2022-12-31T23:59:00",
+      "dueDate": "2023-12-31T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2480381/submissions/new",
       "submitted": false
     },
     {
       "title": "[Ungraded] HW8 Practice Copy",
       "course": "CS 188",
-      "dueDate": "2022-12-31T23:59:00",
+      "dueDate": "2023-12-31T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2480382/submissions/new",
       "submitted": false
     },
     {
       "title": "[Ungraded] HW9 Practice Copy",
       "course": "CS 188",
-      "dueDate": "2022-12-31T23:59:00",
+      "dueDate": "2023-12-31T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2480383/submissions/new",
       "submitted": false
     },
     {
       "title": "Course Evaluations",
       "course": "CS 188",
-      "dueDate": "2022-12-31T23:59:00",
+      "dueDate": "2023-12-31T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2480395/submissions/153658826",
       "submitted": true
     },
     {
       "title": "[Ungraded] Homework 10 (Electronic)",
       "course": "CS 188",
-      "dueDate": "2022-12-31T23:59:00",
+      "dueDate": "2023-12-31T23:59:00",
       "link": "https://www.gradescope.com/courses/419636/assignments/2482267/submissions/new",
       "submitted": false
     },
     {
       "title": "[Ungraded] Demo Online Final Exam",
       "course": "CS 188",
-      "dueDate": "2022-12-17T16:00:00",
+      "dueDate": "2023-12-17T16:00:00",
       "link": "https://www.gradescope.com/courses/419636",
       "submitted": false
     },
     {
       "title": "Final Exam (Online)",
       "course": "CS 188",
-      "dueDate": "2022-12-15T14:40:00",
+      "dueDate": "2023-12-15T14:40:00",
       "link": "https://www.gradescope.com/courses/419636",
       "submitted": false
     }
@@ -341,119 +341,119 @@ assignments = {
     {
       "title": "Project 1",
       "course": "Data 101",
-      "dueDate": "2022-09-23T23:59:00",
+      "dueDate": "2023-09-23T23:59:00",
       "link": "https://www.gradescope.com/courses/421137/assignments/2227686/submissions/134138458",
       "submitted": true
     },
     {
       "title": "Multivitamin 1",
       "course": "Data 101",
-      "dueDate": "2022-09-30T23:59:00",
+      "dueDate": "2023-09-30T23:59:00",
       "link": "https://www.gradescope.com/courses/421137/assignments/2255543/submissions/134790273",
       "submitted": true
     },
     {
       "title": "Project 2",
       "course": "Data 101",
-      "dueDate": "2022-10-10T23:59:00",
+      "dueDate": "2023-10-10T23:59:00",
       "link": "https://www.gradescope.com/courses/421137/assignments/2270476/submissions/138388461",
       "submitted": true
     },
     {
       "title": "Multivitamin 2",
       "course": "Data 101",
-      "dueDate": "2022-10-14T23:59:00",
+      "dueDate": "2023-10-14T23:59:00",
       "link": "https://www.gradescope.com/courses/421137/assignments/2273057/submissions/139121409",
       "submitted": true
     },
     {
       "title": "Multivitamin 3",
       "course": "Data 101",
-      "dueDate": "2022-10-28T23:59:00",
+      "dueDate": "2023-10-28T23:59:00",
       "link": "https://www.gradescope.com/courses/421137/assignments/2322741/submissions/142216698",
       "submitted": true
     },
     {
       "title": "Project 3",
       "course": "Data 101",
-      "dueDate": "2022-10-28T23:59:00",
+      "dueDate": "2023-10-28T23:59:00",
       "link": "https://www.gradescope.com/courses/421137/assignments/2346655/submissions/143629613",
       "submitted": true
     },
     {
       "title": "Multivitamin 4",
       "course": "Data 101",
-      "dueDate": "2022-11-04T23:59:00",
+      "dueDate": "2023-11-04T23:59:00",
       "link": "https://www.gradescope.com/courses/421137/assignments/2362462/submissions/145119992",
       "submitted": true
     },
     {
       "title": "Project 4",
       "course": "Data 101",
-      "dueDate": "2022-12-02T23:59:00",
+      "dueDate": "2023-12-02T23:59:00",
       "link": "https://www.gradescope.com/courses/421137/assignments/2392143/submissions/148885008",
       "submitted": true
     },
     {
       "title": "Project 4 [CHECKPOINT]",
       "course": "Data 101",
-      "dueDate": "2022-11-10T23:59:00",
+      "dueDate": "2023-11-10T23:59:00",
       "link": "https://www.gradescope.com/courses/421137/assignments/2401854/submissions/147610383",
       "submitted": true
     },
     {
       "title": "Multivitamin 5",
       "course": "Data 101",
-      "dueDate": "2022-12-02T23:59:00",
+      "dueDate": "2023-12-02T23:59:00",
       "link": "https://www.gradescope.com/courses/421137/assignments/2404589/submissions/148885644",
       "submitted": true
     },
     {
       "title": "Multivitamin 1 [PRACTICE]",
       "course": "Data 101",
-      "dueDate": "2022-12-16T00:00:00",
+      "dueDate": "2023-12-16T00:00:00",
       "link": "https://www.gradescope.com/courses/421137/assignments/2488368/submissions/154056623",
       "submitted": true
     },
     {
       "title": "Multivitamin 2 [PRACTICE]",
       "course": "Data 101",
-      "dueDate": "2022-12-16T00:00:00",
+      "dueDate": "2023-12-16T00:00:00",
       "link": "https://www.gradescope.com/courses/421137/assignments/2488370/submissions/154103739",
       "submitted": true
     },
     {
       "title": "Multivitamin 3 [PRACTICE]",
       "course": "Data 101",
-      "dueDate": "2022-12-16T23:59:00",
+      "dueDate": "2023-12-16T23:59:00",
       "link": "https://www.gradescope.com/courses/421137/assignments/2488371/submissions/154106729",
       "submitted": true
     },
     {
       "title": "Multivitamin 4 [PRACTICE]",
       "course": "Data 101",
-      "dueDate": "2022-12-16T23:59:00",
+      "dueDate": "2023-12-16T23:59:00",
       "link": "https://www.gradescope.com/courses/421137/assignments/2488373/submissions/154499519",
       "submitted": true
     },
     {
       "title": "Multivitamin 5 [PRACTICE]",
       "course": "Data 101",
-      "dueDate": "2022-12-16T23:59:00",
+      "dueDate": "2023-12-16T23:59:00",
       "link": "https://www.gradescope.com/courses/421137/assignments/2488375/submissions/154500411",
       "submitted": true
     },
     {
       "title": "Final Grade",
       "course": "Data 101",
-      "dueDate": "2022-12-18T23:59:00",
+      "dueDate": "2023-12-18T23:59:00",
       "link": "https://www.gradescope.com/courses/421137/assignments/2502002/submissions/153880005",
       "submitted": true
     },
     {
       "title": "FINAL EXAM",
       "course": "Data 101",
-      "dueDate": "2022-12-15T14:30:00",
+      "dueDate": "2023-12-15T14:30:00",
       "link": "https://www.gradescope.com/courses/421137/assignments/2504946/submissions/154559841",
       "submitted": true
     }
